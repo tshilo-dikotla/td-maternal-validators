70fbad1d830a1060a71127311c4b3a6d4d11ef0f
Leslie <leslie.kep@gmail.com>
2018-02-14 12:32:34 +0200

    initial

---
+++ setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup
+from setuptools import find_packages
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='tshilo-dikotla maternal validators',
+    version='0.1.0',
+    author=u'lkepaletswe',
+    author_email='leslie.kep@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/tshilo-dikotla/td-maternal',
+    license='GPL license, see LICENSE',
+    description='Tshilo Dikotla.',
+    long_description=README,
+    zip_safe=False,
+    keywords='django tshilo dikotla',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
---
+++ td_maternal_validators/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig as DjangoApponfig
+
+
+class AppConfig(DjangoApponfig):
+    name = 'td_maternal_validators'
+    verbose_name = 'Tshilo Dikotla Maternal Form Validators'
