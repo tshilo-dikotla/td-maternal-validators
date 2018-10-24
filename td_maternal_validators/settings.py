1837dbf776debfb5e1d05e445f78a1a085ad9d95
Titus Diphale <tdiphale@bhp.org.bw>
2018-10-24 10:20:02 +0200

    merge conflict settings.py

--- td_maternal_validators/settings.py
+++ td_maternal_validators/settings.py
@@ -5,7 +5,7 @@ import sys
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 APP_NAME = 'td_maternal_validators'
 SITE_ID = 40
-REVIEWER_SITE_ID = 40
+REVIEWER_SITE_ID = 0
 ETC_DIR = os.path.join(BASE_DIR, 'etc')
 
 # RANDOMIZATION_LIST_PATH = os.path.join(BASE_DIR, 'test_randomization_list.csv')
