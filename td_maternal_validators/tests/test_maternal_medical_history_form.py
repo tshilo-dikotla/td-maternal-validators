3c52e629c91771cb05df2bd8576c2bafd6c1417e
Deji <soluwamayowa@bhp.org.bw>
2019-01-23 21:36:19 +0200

    fixing maternal medical history and writing more tests

--- td_maternal_validators/tests/test_maternal_medical_history_form.py
+++ td_maternal_validators/tests/test_maternal_medical_history_form.py
@@ -4,7 +4,7 @@ from django.test import TestCase, tag
 from edc_base.utils import get_utcnow
 from edc_constants.constants import POS, NOT_APPLICABLE, MALE, YES, NO, NEG
 from .models import (
-    MaternalConsent, Appointment, MaternalVisit, RapidTestResult, AntenatalEnrollment,
+    MaternalConsent, Appointment, MaternalVisit,
     RegisteredSubject, ListModel)
 from ..form_validators import MaternalMedicalHistoryFormValidator
 from td_maternal.models.list_models import ChronicConditions, MaternalMedications, WcsDxAdult
@@ -243,7 +243,7 @@ class TestMaternalMedicalHistoryForm(TestCase):
         self.assertIn('who', form_validator._errors)
 
     def test_if_who_diagnosis_who_chronic_is_invalid_none(self):
-        '''True if subject_status is NEG and who_dianosis is NOT_APPLICABLE
+        '''True if subject_status is NEG and who_dianosis is none
         '''
         maternal_status = MaternalStatusHelper(status=NEG)
         MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
@@ -259,7 +259,8 @@ class TestMaternalMedicalHistoryForm(TestCase):
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
     def test_if_who_diagnosis_who_chronic_is_invalid_pos(self):
-        '''True if subject_status is POS and who_dianosis is NOT_APPLICABLE
+        '''Assert raises exception if subject_status is NEG and who_dianosis
+        is NOT_APPLICABLE
         '''
         maternal_status = MaternalStatusHelper(status=NEG)
         MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
@@ -273,7 +274,7 @@ class TestMaternalMedicalHistoryForm(TestCase):
         self.assertIn('who', form_validator._errors)
 
     def test_if_mother_medication_true(self):
-        '''True if mother_medication is selected
+        '''True if mother_medication is true
         '''
         maternal_status = MaternalStatusHelper(status=NEG)
         MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
@@ -287,23 +288,9 @@ class TestMaternalMedicalHistoryForm(TestCase):
         except ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
-    def test_if_mother_medication_false(self):
-        '''True if mother_medication is selected
-        '''
-        maternal_status = MaternalStatusHelper(status=POS)
-        MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
-        self.cleaned_data.update(
-            mother_medication=False,
-        )
-        form_validator = MaternalMedicalHistoryFormValidator(
-            cleaned_data=self.cleaned_data)
-        try:
-            form_validator.validate()
-        except ValidationError as e:
-            self.fail(f'ValidationError unexpectedly raised. Got{e}')
-
     def test_validate_seropositive_invalid(self):
-        '''True if sero_posetive is yes and date_hiv_diagnosis is not given
+        '''Assert raises exception if sero_posetive is yes
+        and date_hiv_diagnosis is not given
         '''
         maternal_status = MaternalStatusHelper(status=NEG)
         MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
@@ -317,8 +304,8 @@ class TestMaternalMedicalHistoryForm(TestCase):
         self.assertIn('sero_posetive', form_validator._errors)
 
     def test_validate_positive_mother_seropositive_yes(self):
-        '''True if sero_posetive is yes and perinataly_infected, know_hiv_status,
-        lowest_cd4_known are given
+        '''Assert raises exception if sero_posetive is yes and perinataly_infected,
+        know_hiv_status, lowest_cd4_known are given
         '''
         maternal_status = MaternalStatusHelper(status=NEG)
         MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
@@ -334,8 +321,8 @@ class TestMaternalMedicalHistoryForm(TestCase):
         self.assertIn('sero_posetive', form_validator._errors)
 
     def test_validate_positive_mother_seropositive_no(self):
-        '''True if sero_posetive is yes and perinataly_infected, know_hiv_status,
-        lowest_cd4_known are given
+        '''True if sero_posetive is no and perinataly_infected, know_hiv_status,
+        lowest_cd4_known are not applicable
         '''
         maternal_status = MaternalStatusHelper(status=NEG)
         MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
@@ -353,8 +340,8 @@ class TestMaternalMedicalHistoryForm(TestCase):
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
     def test_validate_negative_mother_seropositive_no_cd4_not(self):
-        '''True if sero_posetive is no and perinataly_infected, know_hiv_status,
-        lowest_cd4_known are given
+        '''True if sero_posetive is no and perinataly_infected,
+        know_hiv_status are not given, but lowest_cd4_known is given
         '''
         maternal_status = MaternalStatusHelper(status=NEG)
         MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
@@ -373,8 +360,8 @@ class TestMaternalMedicalHistoryForm(TestCase):
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
     def test_validate_negative_no_cd4_not(self):
-        '''True if sero_posetive is yes and perinataly_infected, know_hiv_status,
-        lowest_cd4_known are given
+        '''Assert raises exception if sero_posetive is yes and perinataly_infected,
+        know_hiv_status, lowest_cd4_known are given
         '''
         maternal_status = MaternalStatusHelper(status=NEG)
         MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
@@ -392,7 +379,7 @@ class TestMaternalMedicalHistoryForm(TestCase):
 
     def test_validate_negative_no_cd4_invalid(self):
         '''True if sero_posetive is no and perinataly_infected, know_hiv_status,
-        lowest_cd4_known are given
+        lowest_cd4_known are not given
         '''
         maternal_status = MaternalStatusHelper(status=POS)
         MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
@@ -410,8 +397,7 @@ class TestMaternalMedicalHistoryForm(TestCase):
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
 
     def test_negative_mother_seropositive_no(self):
-        '''True if sero_posetive is yes and perinataly_infected, know_hiv_status,
-        lowest_cd4_known are given
+        '''Assert raises exception if sero_posetive is yes
         '''
         maternal_status = MaternalStatusHelper(status=NEG)
         MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
@@ -422,3 +408,94 @@ class TestMaternalMedicalHistoryForm(TestCase):
             cleaned_data=self.cleaned_data)
         self.assertRaises(ValidationError, form_validator.validate)
         self.assertIn('sero_posetive', form_validator._errors)
+
+    def test_validate_seropositive_date_hiv_diagnosis(self):
+        '''Assert raises exception if sero_posetive is yes and date_hiv_diagnosis is
+        given
+        '''
+        maternal_status = MaternalStatusHelper(status=NEG)
+        MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
+        self.cleaned_data.update(
+            sero_posetive=YES,
+            date_hiv_diagnosis=get_utcnow()
+        )
+        form_validator = MaternalMedicalHistoryFormValidator(
+            cleaned_data=self.cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('sero_posetive', form_validator._errors)
+
+    def test_validate_seropositive_date_hiv_diagnosis_valid(self):
+        '''True if sero_posetive is yes and date_hiv_diagnosis is not given
+        '''
+        maternal_status = MaternalStatusHelper(status=NEG)
+        MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
+        self.cleaned_data.update(
+            sero_posetive=NO,
+            date_hiv_diagnosis=None
+        )
+        form_validator = MaternalMedicalHistoryFormValidator(
+            cleaned_data=self.cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+
+    def test_validate_seropositive_perinataly_infected_valid(self):
+        '''True if sero_posetive is yes and date_hiv_diagnosis is not given
+        '''
+        maternal_status = MaternalStatusHelper(status=NEG)
+        MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
+        self.cleaned_data.update(
+            sero_posetive=NO,
+            perinataly_infected=NOT_APPLICABLE
+        )
+        form_validator = MaternalMedicalHistoryFormValidator(
+            cleaned_data=self.cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError unexpectedly raised. Got{e}')
+
+    def test_validate_seropositive_perinataly_infected_invalid(self):
+        '''Assert raises exception if sero_posetive is yes and perinataly_infected
+        '''
+        maternal_status = MaternalStatusHelper(status=NEG)
+        MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
+        self.cleaned_data.update(
+            sero_posetive=YES,
+            perinataly_infected=YES
+        )
+        form_validator = MaternalMedicalHistoryFormValidator(
+            cleaned_data=self.cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('sero_posetive', form_validator._errors)
+
+    def test_validate_seropositive_know_hiv_status_invalid(self):
+        '''Assert raises exception if sero_posetive is yes and know_hiv_status is yes
+        '''
+        maternal_status = MaternalStatusHelper(status=NEG)
+        MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
+        self.cleaned_data.update(
+            sero_posetive=YES,
+            know_hiv_status=YES
+        )
+        form_validator = MaternalMedicalHistoryFormValidator(
+            cleaned_data=self.cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
+        self.assertIn('sero_posetive', form_validator._errors)
+
+    def test_validate_seropositive_know_hiv_status_valid(self):
+        '''True if sero_posetive is yes and know_hiv_status is NOT applicable
+        '''
+        maternal_status = MaternalStatusHelper(status=NEG)
+        MaternalMedicalHistoryFormValidator.maternal_status_helper = maternal_status
+        self.cleaned_data.update(
+            sero_posetive=NO,
+            know_hiv_status=NOT_APPLICABLE
+        )
+        form_validator = MaternalMedicalHistoryFormValidator(
+            cleaned_data=self.cleaned_data)
+        try:
+            form_validator.validate()
+        except ValidationError as e:
+            self.fail(f'ValidationError unexpectedly raised. Got{e}')
