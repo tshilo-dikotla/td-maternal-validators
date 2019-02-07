15d5532da4130602766ca087b0eb18a09920978f
tumiemosweu <imosweuk@gmail.com>
2019-02-07 15:29:51 +0200

    fix td consent version

--- td_maternal_validators/form_validators/antenatal_visit_membership_form_validation.py
+++ td_maternal_validators/form_validators/antenatal_visit_membership_form_validation.py
@@ -8,3 +8,12 @@ class AntenatalVisitMembershipFormValidator(TDFormValidatorMixin, FormValidator)
     def clean(self):
         self.validate_against_consent_datetime(
             self.cleaned_data.get('report_datetime'))
+
+    @property
+    def subject_screening(self):
+        cleaned_data = self.cleaned_data
+        try:
+            return self.subject_screening_cls.objects.get(
+                screening_identifier=cleaned_data.get('screening_identifier'))
+        except self.subject_screening_cls.DoesNotExist:
+            return None
