8629998047c6be4c84b37a3e78a6850bdaeb54ec
tumiemosweu <imosweuk@gmail.com>
2019-02-19 15:22:57 +0200

    update first name validation consent

--- td_maternal_validators/form_validators/subject_consent_form_validation.py
+++ td_maternal_validators/form_validators/subject_consent_form_validation.py
@@ -51,7 +51,7 @@ class SubjectConsentFormValidator(FormValidator):
         first_name = cleaned_data.get("first_name")
         last_name = cleaned_data.get("last_name")
 
-        if not re.match(r'^[A-Z]+[ ][A-Z]+$', first_name):
+        if not re.match(r'^[A-Z]+|([A-Z]+[ ][A-Z]+)$', first_name):
             message = {'first_name': 'Ensure first name is letters (A-Z) in '
                        'upper case, no special characters, except spaces.'}
             self._errors.update(message)
