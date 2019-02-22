dfe3165b43cc5310ef43681779744894c07a3c24
Leslie <leslie.kep@gmail.com>
2019-02-22 16:31:51 +0200

    update rapid test validations

--- td_maternal_validators/form_validators/rapid_test_result_form_validation.py
+++ td_maternal_validators/form_validators/rapid_test_result_form_validation.py
@@ -1,19 +1,31 @@
-from edc_form_validators import FormValidator
+from django.core.exceptions import ValidationError
 from edc_constants.constants import YES
+from edc_form_validators import FormValidator
 
 
 class RapidTestResultFormValidator(FormValidator):
 
     def clean(self):
-        required_fields = ('result_date', 'result')
-        for required in required_fields:
-            self.required_if(
-                YES,
-                field='rapid_test_done',
-                field_required=required,
-                required_msg=('If a rapid test was processed, what is '
-                              f'the {required} of the rapid test?'),
-                not_required_msg=('If a rapid test was not processed, '
-                                  f'please do not provide the {required}. '
-                                  'Got {}.'.format(self.cleaned_data.get(required)))
-            )
+
+        if self.cleaned_data.get('rapid_test_done') == YES:
+            if not self.cleaned_data.get('result_date'):
+                msg = {'result_date':
+                       'This field is required'}
+                self._errors.update(msg)
+                raise ValidationError(msg)
+            if not self.cleaned_data.get('result'):
+                msg = {'result':
+                       'This field is required'}
+                self._errors.update(msg)
+                raise ValidationError(msg)
+        else:
+            if self.cleaned_data.get('result_date'):
+                msg = {'result_date':
+                       'This field is not required'}
+                self._errors.update(msg)
+                raise ValidationError(msg)
+            if self.cleaned_data.get('result'):
+                msg = {'result':
+                       'This field is not required'}
+                self._errors.update(msg)
+                raise ValidationError(msg)
