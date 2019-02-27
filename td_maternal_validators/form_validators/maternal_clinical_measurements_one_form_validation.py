b646d3d081615428b2f928490c790499c7e86a93
tumiemosweu <imosweuk@gmail.com>
2019-02-27 18:01:37 +0200

    cater for none clinical measurements one

--- td_maternal_validators/form_validators/maternal_clinical_measurements_one_form_validation.py
+++ td_maternal_validators/form_validators/maternal_clinical_measurements_one_form_validation.py
@@ -1,14 +1,16 @@
-from edc_form_validators import FormValidator
 from django.core.exceptions import ValidationError
+from edc_form_validators import FormValidator
 
 
 class MaternalClinicalMeasurememtsOneFormValidator(FormValidator):
+
     def clean(self):
         cleaned_data = self.cleaned_data
-        if cleaned_data.get('systolic_bp') < \
-                cleaned_data.get('diastolic_bp'):
-            msg = {'diastolic_bp': 'Systolic blood pressure cannot be lower '
-                                   'than the diastolic blood pressure. '
-                                   'Please correct.'}
-            self._errors.update(msg)
-            raise ValidationError(msg)
+        if (cleaned_data.get('systolic_bp') and cleaned_data.get('diastolic_bp')):
+            if cleaned_data.get('systolic_bp') < \
+                    cleaned_data.get('diastolic_bp'):
+                msg = {'diastolic_bp': 'Systolic blood pressure cannot be lower '
+                                       'than the diastolic blood pressure. '
+                                       'Please correct.'}
+                self._errors.update(msg)
+                raise ValidationError(msg)
