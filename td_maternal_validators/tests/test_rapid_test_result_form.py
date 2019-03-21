5df29822db10c5b349b1f38c9da45f63289d6cab
Leslie <leslie.kep@gmail.com>
2019-03-21 17:34:12 +0200

    add rapid test validator and override appointment validator

--- td_maternal_validators/tests/test_rapid_test_result_form.py
+++ td_maternal_validators/tests/test_rapid_test_result_form.py
@@ -1,12 +1,18 @@
+from dateutil.relativedelta import relativedelta
 from django.core.exceptions import ValidationError
 from django.test import TestCase, tag
 from edc_base.utils import get_utcnow
 from edc_constants.constants import YES, POS, NEG, IND, NO
+
 from ..form_validators import RapidTestResultFormValidator
+from .models import AntenatalEnrollment, Appointment, MaternalVisit
 
 
 @tag('rr')
 class TestRapidTestResultForm(TestCase):
+
+    def setUp(self):
+        RapidTestResultFormValidator.antenatal_enrollment_model = 'td_maternal_validators.antenatalenrollment'
 
     def test_rapid_test_done_YES_result_date_required(self):
         cleaned_data = {
@@ -91,3 +97,33 @@ class TestRapidTestResultForm(TestCase):
             form_validator.validate()
         except ValidationError as e:
             self.fail(f'ValidationError unexpectedly raised. Got{e}')
+
+    @tag('rt')
+    def test_antenatal_enrollement_rapid_test(self):
+        subject_identifier = '11111111'
+        self.antenatal_enrollment = AntenatalEnrollment.objects.create(
+            subject_identifier=subject_identifier,
+            week32_test=NEG,
+            enrollment_hiv_status=NEG,
+            week32_result=NEG,
+            rapid_test_done=YES,
+            rapid_test_result=NEG,
+            rapid_test_date=get_utcnow().date(),
+            report_datetime=get_utcnow())
+
+        appointment = Appointment.objects.create(
+            subject_identifier=subject_identifier,
+            appt_datetime=get_utcnow(),
+            visit_code='1000')
+
+        maternal_visit = MaternalVisit.objects.create(
+            appointment=appointment,
+            subject_identifier=subject_identifier)
+        cleaned_data = {
+            'maternal_visit': maternal_visit,
+            'rapid_test_done': YES,
+            'result_date': (get_utcnow() - relativedelta(months=4)).date(),
+            'result': NEG}
+        form_validator = RapidTestResultFormValidator(
+            cleaned_data=cleaned_data)
+        self.assertRaises(ValidationError, form_validator.validate)
