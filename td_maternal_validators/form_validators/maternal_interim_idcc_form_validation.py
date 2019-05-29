0de2085af7faee436c6482a3a0d523531749d34d
MoffatMore <mofenyimoffat@gmail.com>
2019-05-29 08:52:21 +0200

    changing value of vl_value from 750,000 to 10,000,000

--- td_maternal_validators/form_validators/maternal_interim_idcc_form_validation.py
+++ td_maternal_validators/form_validators/maternal_interim_idcc_form_validation.py
@@ -64,17 +64,17 @@ class MaternalIterimIdccFormValidator(TDCRFFormValidator,
                 self._errors.update(msg)
                 raise ValidationError(msg)
 
-            if (vl_value != 750000
+            if (vl_value != 10000000
                     and cleaned_data.get('value_vl_size') == 'greater_than'):
                 msg = {'value_vl': 'You indicated that the value of the most recent VL is '
-                       f'greater_than a {vl_value}, therefore the value of VL should be 750000'}
+                       f'greater_than a {vl_value}, therefore the value of VL should be 10,000,000'}
                 self._errors.update(msg)
                 raise ValidationError(msg)
 
-            if (vl_value > 750000 or vl_value < 400
+            if (vl_value > 10000000 or vl_value < 400
                     and cleaned_data.get('value_vl_size') == 'equal'):
                 msg = {'value_vl': 'You indicated that the value of the '
                        f'most recent VL is equal to a {vl_value}, therefore the value of VL '
-                       'should be between 400 and 750000 (inclusive of 400 and 750,000)'}
+                       'should be between 400 and 750000 (inclusive of 400 and 10,000,000)'}
                 self._errors.update(msg)
                 raise ValidationError(msg)
