05d6bbd7bccaad1127722304ce9303346051549a
tumiemosweu <imosweuk@gmail.com>
2020-03-06 11:28:20 +0200

    remove offstudy validation

--- td_maternal_validators/form_validators/crf_form_validator.py
+++ td_maternal_validators/form_validators/crf_form_validator.py
@@ -11,8 +11,6 @@ class TDCRFFormValidator:
     def clean(self):
         self.validate_against_visit_datetime(
             self.cleaned_data.get('report_datetime'))
-        if self.instance and not self.instance.id:
-            self.validate_offstudy_model()
         super().clean()
 
     def validate_against_visit_datetime(self, report_datetime):
@@ -20,32 +18,3 @@ class TDCRFFormValidator:
                 self.cleaned_data.get('maternal_visit').report_datetime):
             raise forms.ValidationError(
                 "Report datetime cannot be before visit datetime.")
-
-    def validate_offstudy_model(self):
-        maternal_offstudy_cls = django_apps.get_model(
-            'td_prn.maternaloffstudy')
-        action_cls = site_action_items.get(
-            maternal_offstudy_cls.action_name)
-        action_item_model_cls = action_cls.action_item_model_cls()
-
-        try:
-            action_item_model_cls.objects.get(
-                subject_identifier=self.subject_identifier,
-                action_type__name=MATERNALOFF_STUDY_ACTION,
-                status=NEW)
-        except action_item_model_cls.DoesNotExist:
-            try:
-                maternal_offstudy_cls.objects.get(
-                    subject_identifier=self.subject_identifier)
-            except maternal_offstudy_cls.DoesNotExist:
-                pass
-            else:
-                raise forms.ValidationError(
-                    'Participant has been taken offstudy. Cannot capture any '
-                    'new data.')
-        else:
-            self.maternal_visit = self.cleaned_data.get('maternal_visit') or None
-            if not self.maternal_visit or self.maternal_visit.require_crfs == NO:
-                raise forms.ValidationError(
-                    'Participant is scheduled to be taken offstudy without '
-                    'any new data collection. Cannot capture any new data.')
